ee8058af1fefbd2fa2b95cb2ee20db056a5ec2fb
Jonathan <djgrovezone@gmail.com>
2020-12-18 17:38:25 +0100

    Alñadido Layout al proyecto y maquetado de homepage

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,9 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class SecurityController extends AbstractController
 {
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -22,6 +30,8 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        $this->session->getFlashBag()->add('success','Esto es una alerta de prueba');
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
